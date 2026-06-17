8217fab7f7889ba318f72b45a98c7c43631258ed
joab8291-glitch <joab8291@gmail.com>
2026-06-18 02:20:42 +0300

    Create index.tsx

---
+++ src/sections/AirtimePurchaseSection/index.tsx
@@ -0,0 +1,9 @@
+import { AirtimePurchaseFormCard } from "@/sections/AirtimePurchaseSection/components/AirtimePurchaseFormCard";
+
+export const AirtimePurchaseSection = () => {
+  return (
+    <section className="relative caret-transparent flex justify-center outline-[3px] z-[1] mt-0 md:mt-[-100px]">
+      <AirtimePurchaseFormCard />
+    </section>
+  );
+};
