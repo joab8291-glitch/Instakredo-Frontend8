d6dbbe8bf0b4d29b4c3113edf2ba0ff2b837ea73
joab8291-glitch <joab8291@gmail.com>
2026-06-18 02:02:00 +0300

    Create HeaderImage.tsx

---
+++ src/sections/Header/components/HeaderImage.tsx
@@ -0,0 +1,11 @@
+export const HeaderImage = () => {
+  return (
+    <div className="caret-transparent basis-[0%] grow min-h-[auto] min-w-[auto] outline-[3px] text-right mt-0 md:mt-[-60px]">
+      <img
+        src="https://c.animaapp.com/mqh5jtbuMW9pRs/assets/lady.webp"
+        alt="Happy customer using Packson Ventures"
+        className="caret-transparent inline max-w-[500px] outline-[3px] align-baseline w-full mt-5 rounded-[10px] md:max-w-[700px] md:mt-0"
+      />
+    </div>
+  );
+};
