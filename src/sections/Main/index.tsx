f08baff222c6276b51e76b8db0c4735a67c14043
joab8291-glitch <joab8291@gmail.com>
2026-06-18 03:19:34 +0300

    Update index.tsx

--- src/sections/Main/index.tsx
+++ src/sections/Main/index.tsx
@@ -4,7 +4,7 @@ import { CallToActionSection } from "@/sections/CallToActionSection";
 
 export const Main = () => {
   return (
-    <main className="bg-[radial-gradient(circle,rgb(255,255,255)_20%,rgb(255,252,246)_40%,rgb(243,249,254)_60%,rgb(250,251,249)_100%)] caret-transparent outline-[3px] py-[50px]">
+    <main className="caret-transparent outline-[3px] py-[50px]">
       <AirtimePurchaseSection />
       <FeaturesSection />
       <CallToActionSection />
