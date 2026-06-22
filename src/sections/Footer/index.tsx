d67bd6184d18172aadb46cd864e48e72dfb10949
joab8291-glitch <joab8291@gmail.com>
2026-06-22 11:33:24 +0300

    Update index.tsx

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -2,10 +2,10 @@ import { SocialLinks } from "@/sections/Footer/components/SocialLinks";
 
 export const Footer = () => {
   return (
-    <footer className="border-t border-stone-200 bg-stone-50">
+    <footer className="border-t border-amber-400/10">
       <div className="items-center flex flex-col justify-between max-w-[1440px] gap-y-2.5 text-center w-full mx-auto px-5 py-6 md:flex-row md:px-16">
-        <div className="text-stone-500 flex flex-col text-center md:text-start">
-          <p className="font-semibold text-neutral-900">Copyright © InstaKredo</p>
+        <div className="text-[#6b8070] flex flex-col text-center md:text-start">
+          <p className="font-semibold text-white">Copyright © InstaKredo</p>
           <p className="text-xs">Powered by Packson Ventures</p>
         </div>
         <SocialLinks />
