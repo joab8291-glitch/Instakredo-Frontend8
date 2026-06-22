1c414033ffc363e15e31fb1a33621c7b65f00981
joab8291-glitch <joab8291@gmail.com>
2026-06-22 11:45:26 +0300

    Update index.tsx

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="border-t border-amber-400/10">
       <div className="items-center flex flex-col justify-between max-w-[1440px] gap-y-2.5 text-center w-full mx-auto px-5 py-6 md:flex-row md:px-16">
         <div className="text-[#6b8070] flex flex-col text-center md:text-start">
-          <p className="font-semibold text-white">Copyright © InstaKredo</p>
+          <p>Copyright © InstaKredo</p>
           <p className="text-xs">Powered by Packson Ventures</p>
         </div>
         <SocialLinks />
