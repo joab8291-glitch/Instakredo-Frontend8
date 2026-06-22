098d538cfa8ce0dccebaf8faee506d700b579384
joab8291-glitch <joab8291@gmail.com>
2026-06-22 17:10:01 +0300

    Update index.tsx

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -2,11 +2,11 @@ import { SocialLinks } from "@/sections/Footer/components/SocialLinks";
 
 export const Footer = () => {
   return (
-    <footer className="border-t border-amber-400/10">
-      <div className="items-center flex flex-col justify-between max-w-[1440px] gap-y-2.5 text-center w-full mx-auto px-5 py-6 md:flex-row md:px-16">
-        <div className="text-[#6b8070] flex flex-col text-center md:text-start">
+    <footer className="caret-transparent outline-[3px]">
+      <div className="items-center box-border caret-transparent gap-x-2.5 flex flex-col justify-between max-w-[1440px] outline-[3px] gap-y-2.5 text-center w-full mx-auto px-5 md:gap-x-5 md:flex-row md:gap-y-5 md:text-start md:px-16">
+        <div className="text-stone-700 items-center caret-transparent flex flex-col h-full min-h-[auto] min-w-[auto] opacity-80 outline-[3px] text-center md:text-start">
           <p>Copyright © InstaKredo</p>
-          <p className="text-xs">Powered by Packson Ventures</p>
+          <p className="text-xs text-stone-500">Powered by Packson Ventures</p>
         </div>
         <SocialLinks />
       </div>
